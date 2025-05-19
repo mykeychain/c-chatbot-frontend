309315711843a4c801cd2c974d0d38cdbdcc3dd5
Mike Chang <mchang95@gmail.com>
2025-05-18 22:29:21 -0700

    [MOD] replace 'Send' to an svg icon

--- src/components/MessagePane.tsx
+++ src/components/MessagePane.tsx
@@ -66,10 +66,12 @@ export function MessagePane({ isMenuOpen, setIsMenuOpen, selectedConversation }:
               onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
             />
             <button
-              className={`px-4 py-2 rounded-xl shadow ${isDarkMode ? 'bg-[#17686c] text-[#e0e1dd]' : 'bg-[#a2c9a6] text-gray-800'}`}
+              className={`px-4 py-2 rounded-xl shadow cursor-pointer ${isDarkMode ? 'bg-[#17686c] text-[#e0e1dd]' : 'bg-[#a2c9a6] text-gray-800'}`}
               onClick={sendMessage}
             >
-              Send
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+              </svg>
             </button>
           </div>
         </>
