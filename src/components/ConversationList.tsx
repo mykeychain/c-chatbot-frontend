80ca3db3c18b4e1db85881b59c05a8c717e9249b
Mike Chang <mchang95@gmail.com>
2025-05-29 18:01:06 -0700

    [MOD] sidebar menu toggle button back to the triple lines rather than sidebar icon

--- src/components/ConversationList.tsx
+++ src/components/ConversationList.tsx
@@ -51,8 +51,8 @@ export function ConversationList({
         </div>
         {/* Close Sidebar Button */}
         <button className="text-sm underline cursor-pointer" onClick={() => setIsMenuOpen(false)}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="size-6" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2z"/>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
       </div>
